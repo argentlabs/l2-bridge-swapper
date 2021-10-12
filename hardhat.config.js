66c542a7d24372bc01064d84648837bd70b4435d
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2021-10-12 15:03:34 +0200

    Use KMS private keys

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
+require("@rumblefishdev/hardhat-kms-signer");
 require("dotenv").config();
 
 /**
@@ -9,13 +10,28 @@ require("dotenv").config();
   networks: {
     hardhat: {
       forking: {
-        url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
       },
     },
     dev: {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
-      accounts: [`0x${process.env.DEV_PKEY}`],
+      kmsKeyId: process.env.DEV_KMSID,
       chainId: 4,
+    },
+    test: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+      kmsKeyId: process.env.TEST_KMSID,
+      chainId: 3,
+    },
+    staging: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+      kmsKeyId: process.env.STAGING_KMSID,
+      chainId: 1,
+    },
+    prod: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+      kmsKeyId: process.env.PROD_KMSID,
+      chainId: 1,
     },
   },
   solidity: {
