54f4e64dcee71d2a21b565200a8630ecbee510de
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2021-12-14 16:31:26 +0100

    Aave bridge swapper with basic integration test

--- test-integration/lib/balance-monkey-patch.ts
+++ test-integration/lib/balance-monkey-patch.ts
@@ -6,15 +6,44 @@ const probeAddress = ethers.constants.AddressZero;
 const probeAmount1 = abi.encode(["uint256"], [1]);
 const probeAmount2 = abi.encode(["uint256"], [2]);
 
+const cache: Record<string, number> = {};
+
 /**
  * @param mappingSlot Slot index of `balances` in the token contract
  * @param account The account we want the balance from
  * @returns The slot number used to store `balances[account]` if the `balances` mapping is
  * stored at slot `mappingSlot`
  */
-const balanceSlotAtIndex = (mappingSlot: number, account: string) => {
+const balanceValueSlot = (mappingSlot: number, account: string) => {
   const balanceSlot = ethers.utils.keccak256(abi.encode(["address", "uint256"], [account, mappingSlot]));
-  return balanceSlot.replace("0x0", "0x"); // hardhat doesn't like zero-padding
+  return balanceSlot.replace(/^0x0+/, "0x"); // hardhat doesn't like zero-padding
+};
+
+/**
+ * Finds the storage slot of the mapping holding ERC-20 balances, if such a simple mapping is
+ * used in this contract. Throws otherwise.
+ * @param tokenAddress ERC-20 address
+ * @returns Storage slot number for the balances mapping
+ */
+const findBalancesSlot = async (tokenAddress: string) => {
+  const token = await ethers.getContractAt("IERC20", tokenAddress);
+
+  for (let index = 0; index < 100; index++) {
+    const probedSlot = balanceValueSlot(index, probeAddress);
+
+    const prevAmount = await hre.network.provider.send("eth_getStorageAt", [tokenAddress, probedSlot, "latest"]);
+    const probeAmount = prevAmount === probeAmount1 ? probeAmount2 : probeAmount1;
+
+    await hre.network.provider.send("hardhat_setStorageAt", [tokenAddress, probedSlot, probeAmount]);
+    const maybeChangedBalance = await token.balanceOf(probeAddress);
+    await hre.network.provider.send("hardhat_setStorageAt", [tokenAddress, probedSlot, prevAmount]);
+
+    if (maybeChangedBalance.eq(probeAmount)) {
+      return index;
+    }
+  }
+
+  throw "Balances slot not found!";
 };
 
 /**
@@ -24,26 +53,13 @@ const balanceSlotAtIndex = (mappingSlot: number, account: string) => {
  * @param balance The new balance
  */
 export const setTokenBalance = async (tokenAddress: string, accountAddress: string, balance: BigNumberish) => {
-  const token = await ethers.getContractAt("IERC20", tokenAddress);
-
-  for (let index = 0; index < 100; index++) {
-    const probedSlot = balanceSlotAtIndex(index, probeAddress);
-
-    const prevAmount = await hre.network.provider.send("eth_getStorageAt", [tokenAddress, probedSlot, "latest"]);
-    const probeAmount = prevAmount === probeAmount1 ? probeAmount2 : probeAmount1;
-
-    await hre.network.provider.send("hardhat_setStorageAt", [tokenAddress, probedSlot, probeAmount]);
-
-    const maybeChangedBalance = await token.balanceOf(probeAddress);
-    if (maybeChangedBalance.eq(probeAmount)) {
-      const balanceSlot = balanceSlotAtIndex(index, accountAddress);
-      const balanceAmount = abi.encode(["uint256"], [ethers.BigNumber.from(balance)]);
-      await hre.network.provider.send("hardhat_setStorageAt", [tokenAddress, balanceSlot, balanceAmount]);
-      return;
-    } else {
-      await hre.network.provider.send("hardhat_setStorageAt", [tokenAddress, probedSlot, prevAmount]);
-    }
+  let slot = cache[tokenAddress];
+  if (!slot) {
+    slot = await findBalancesSlot(tokenAddress);
+    cache[tokenAddress] = slot;
   }
 
-  throw "Balances slot not found!";
+  const balanceSlot = balanceValueSlot(slot, accountAddress);
+  const balanceAmount = abi.encode(["uint256"], [ethers.BigNumber.from(balance)]);
+  await hre.network.provider.send("hardhat_setStorageAt", [tokenAddress, balanceSlot, balanceAmount]);
 };
